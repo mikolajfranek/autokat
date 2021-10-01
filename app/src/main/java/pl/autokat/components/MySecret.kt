468635b452a764eb8f83344bdb6b1cdd3281873d
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2021-10-01 21:55:37 +0200

    1.0.6 - refactor

--- app/src/main/java/pl/autokat/components/MySecret.kt
+++ app/src/main/java/pl/autokat/components/MySecret.kt
@@ -1,42 +1,48 @@
 package pl.autokat.components
 
-class MySecret{
-    companion object{
+class MySecret {
+    companion object {
         //spreadsheet id login
-        val SPREADSHEET_ID_LOGIN : String = ""
-        val SPREADSHEET_ID_LOGIN_DEV : String = ""
-        fun getSpreadsheetIdLogin() : String {
-            if(MyConfiguration.PRODUCTION){
-                return SPREADSHEET_ID_LOGIN
+        private const val SPREADSHEET_ID_LOGIN: String = ""
+        private const val SPREADSHEET_ID_LOGIN_DEV: String = ""
+        fun getSpreadsheetIdLogin(): String {
+            if (MyConfiguration.PRODUCTION) {
+                return this.SPREADSHEET_ID_LOGIN
             }
-            return SPREADSHEET_ID_LOGIN_DEV
+            return this.SPREADSHEET_ID_LOGIN_DEV
         }
+
         //spreadsheet id catalyst
-        val SPREADSHEET_ID_CATALYST : String = ""
-        val SPREADSHEET_ID_CATALYST_DEV : String = ""
-        fun getSpreadsheetIdCatalyst() : String {
-            if(MyConfiguration.PRODUCTION){
-                return SPREADSHEET_ID_CATALYST
+        private const val SPREADSHEET_ID_CATALYST: String = ""
+        private const val SPREADSHEET_ID_CATALYST_DEV: String = ""
+        fun getSpreadsheetIdCatalyst(): String {
+            if (MyConfiguration.PRODUCTION) {
+                return this.SPREADSHEET_ID_CATALYST
             }
-            return SPREADSHEET_ID_CATALYST_DEV
+            return this.SPREADSHEET_ID_CATALYST_DEV
         }
+
         //private key for creating jwt
-        val JWT_PRIVATE_KEY : String = ""
-        val JWT_PRIVATE_KEY_DEV : String = ""
-        fun getPrivateKey() : String {
-            if(MyConfiguration.PRODUCTION){
-                return JWT_PRIVATE_KEY
+        private const val JWT_PRIVATE_KEY: String =
+            ""
+        private const val JWT_PRIVATE_KEY_DEV: String =
+            ""
+
+        fun getPrivateKey(): String {
+            if (MyConfiguration.PRODUCTION) {
+                return this.JWT_PRIVATE_KEY
             }
-            return JWT_PRIVATE_KEY_DEV
+            return this.JWT_PRIVATE_KEY_DEV
         }
+
         //email for creating jwt
-        val JWT_EMAIL : String = ""
-        val JWT_EMAIL_DEV : String = ""
-        fun getEmail() : String {
-            if(MyConfiguration.PRODUCTION){
-                return JWT_EMAIL
+        private const val JWT_EMAIL: String = ""
+        private const val JWT_EMAIL_DEV: String = ""
+        fun getEmail(): String {
+            if (MyConfiguration.PRODUCTION) {
+                return this.JWT_EMAIL
             }
-            return JWT_EMAIL_DEV
+            return this.JWT_EMAIL_DEV
         }
     }
 }
