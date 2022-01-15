7de992335740a5bfacfca6780d1d654b32fd06b2
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2022-01-15 13:30:50 +0100

    1.0.7 - refactor

--- app/src/main/java/pl/autokat/UpdateActivity.kt
+++ app/src/main/java/pl/autokat/UpdateActivity.kt
@@ -16,10 +16,6 @@ class UpdateActivity : AppCompatActivity() {
     private var isAvailableUpdateCatalyst: Boolean = false
     private var threadUpdateProgressOfDownloadThumbnail: Thread? = null
 
-    //TODO atomic?
-    private var refreshingDatabase: Boolean = false
-    private var refreshingIsWorking: Boolean = false
-
     //region methods used in override
     private fun init() {
         activityUpdateBinding = ActivityUpdateBinding.inflate(layoutInflater)
@@ -61,9 +57,7 @@ class UpdateActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        refreshingDatabase = false
-
-        //threadUpdateProgressOfDownloadThumbnail.interrupt()
+        stopThreadUpdateProgressOfDownloadThumbnail()
     }
 
     override fun onResume() {
@@ -73,6 +67,14 @@ class UpdateActivity : AppCompatActivity() {
     //endregion
 
     //region methods of turing threads
+    private fun stopThreadUpdateProgressOfDownloadThumbnail() {
+        try {
+            threadUpdateProgressOfDownloadThumbnail?.interrupt()
+        } catch (e: Exception) {
+            //
+        }
+    }
+
     private fun startThreadUpdateProgressOfDownloadThumbnail() {
         if (threadUpdateProgressOfDownloadThumbnail == null) {
             threadUpdateProgressOfDownloadThumbnail =
@@ -80,8 +82,6 @@ class UpdateActivity : AppCompatActivity() {
             threadUpdateProgressOfDownloadThumbnail!!.start()
         }
     }
-    //TODO
-
     //endregion
 
     //region inner classes
@@ -98,21 +98,16 @@ class UpdateActivity : AppCompatActivity() {
                     activityUpdateBinding.notification.text = Configuration.DATABASE_NOT_ACTUAL
                 } else {
                     if (itemsWithUrlThumbnail != 0) {
-                        activityUpdateBinding.progressBar.progress = ((itemsWithThumbnail.toFloat() / itemsWithUrlThumbnail.toFloat()) * 100.toFloat()).toInt()
+                        activityUpdateBinding.progressBar.progress =
+                            ((itemsWithThumbnail.toFloat() / itemsWithUrlThumbnail.toFloat()) * 100.toFloat()).toInt()
                     } else {
                         activityUpdateBinding.progressBar.progress = 0
                     }
                     if (itemsWithThumbnail / itemsFromDatabase != 1) {
-                        val textView = Configuration.BITMAP_STATUS + " (" + itemsWithThumbnail + "/" + itemsWithUrlThumbnail + "/" + itemsFromDatabase + ")"
+                        val textView =
+                            Configuration.BITMAP_STATUS + " (" + itemsWithThumbnail + "/" + itemsWithUrlThumbnail + "/" + itemsFromDatabase + ")"
                         activityUpdateBinding.notification.text = textView
-
-
-
-                        refreshingDatabase = true
-                        //if(threadUpdateProgressOfDownloadThumbnail.state == Thread.State.TERMINATED)
-                        Thread(RunnableUpdateProgressOfDownloadThumbnail()).start()
-
-
+                        startThreadUpdateProgressOfDownloadThumbnail()
                     } else {
                         activityUpdateBinding.notification.text = Configuration.DATABASE_ACTUAL
                     }
@@ -176,49 +171,39 @@ class UpdateActivity : AppCompatActivity() {
         }
     }
 
-
     inner class RunnableUpdateProgressOfDownloadThumbnail : Runnable {
+        private var itemsWithThumbnail: Int = 0
+        private var itemsWithUrlThumbnail: Int = 0
+        private var itemsFromDatabase: Int = 0
 
+        //region methods used in doInBackground
+        private fun onProgressUpdate() {
+            val textView =
+                Configuration.BITMAP_STATUS + " (" + itemsWithThumbnail.toString() + "/" + itemsWithUrlThumbnail.toString() + "/" + itemsFromDatabase.toString() + ")"
+            activityUpdateBinding.notification.text = textView
+            activityUpdateBinding.progressBar.progress =
+                ((itemsWithThumbnail.toFloat() / itemsWithUrlThumbnail.toFloat()) * 100.toFloat()).toInt()
+        }
+        //endregion
 
         override fun run() {
-            //--- onPreExecute
-            //--- doInBackground
             try {
-                val state: Boolean =
-                    refreshingDatabase && refreshingIsWorking == false
-                if (state == true) {
-                    refreshingIsWorking = true
-                    while (refreshingDatabase) {
-
-                        //odświeżanie widoku co 1s
-
-                        Thread.sleep(1000)
-                        val itemsWithThumbnail: Int =
-                            database.getCountCatalystWithThumbnail()
-                        val itemsWithUrlThumbnail: Int =
-                            database.getCountCatalystWithUrlThumbnail()
-                        val itemsFromDatabase: Int =
-                            database.getCountCatalyst()
-                        //--- onProgressUpdate
-                        runOnUiThread {
-                            val textView =
-                                Configuration.BITMAP_STATUS + " (" + itemsWithThumbnail.toString() + "/" + itemsWithUrlThumbnail.toString() + "/" + itemsFromDatabase.toString() + ")"
-                            activityUpdateBinding.notification.text = textView
-                            activityUpdateBinding.progressBar.progress =
-                                ((itemsWithThumbnail.toFloat() / itemsWithUrlThumbnail.toFloat()) * 100.toFloat()).toInt()
-                        }
+                while (Thread.currentThread().isInterrupted == false) {
+                    Thread.sleep(1000)
+                    itemsWithThumbnail = database.getCountCatalystWithThumbnail()
+                    itemsWithUrlThumbnail = database.getCountCatalystWithUrlThumbnail()
+                    itemsFromDatabase = database.getCountCatalyst()
+                    runOnUiThread {
+                        onProgressUpdate()
                     }
-
                 }
             } catch (e: Exception) {
                 //
             } finally {
-                refreshingIsWorking = false
+                threadUpdateProgressOfDownloadThumbnail = null
             }
-            //--- onPostExecute
         }
     }
-
 
     inner class RunnableUpdate(fullUpdateInput: Boolean) : Runnable {
         private var fullUpdate: Boolean = fullUpdateInput
@@ -230,143 +215,130 @@ class UpdateActivity : AppCompatActivity() {
                 Configuration.SPREADSHEET_CATALYST_ID
             ).isEmpty() == false
         }
+
+        private fun onProgressUpdate(progressStep: Float) {
+            activityUpdateBinding.progressBar.progress = progressStep.toInt()
+            activityUpdateBinding.notification.setTextColor(Configuration.COLOR_SUCCESS)
+            activityUpdateBinding.notification.text = Configuration.UPDATE_WAIT
+        }
+
+        private fun getCountOfCatalyst(fullUpdate: Boolean): Pair<Int, Int> {
+            var countDatabase = 0
+            val countSpreadsheet: Int = Spreadsheet.getCountCatalyst()
+            if (fullUpdate == false) {
+                countDatabase = database.getCountCatalyst()
+            }
+            return Pair(countDatabase, countSpreadsheet)
+        }
+
+        @Suppress("SameParameterValue")
+        private fun getBatchJsonArrayOfUpdate(batchSize: Int, countDatabase: Int): JSONArray {
+            val batchJsonArray = JSONArray()
+            val dataCatalysts: JSONArray = Spreadsheet.getDataCatalyst(countDatabase)
+            for (i in 0 until (dataCatalysts.length()) step batchSize) {
+                if (batchJsonArray.isNull(i)) batchJsonArray.put(JSONArray())
+                for (j in i until i + batchSize) {
+                    if (dataCatalysts.isNull(j)) break
+                    if (checkIfRowIsAvailable(
+                            dataCatalysts.getJSONObject(j).getJSONArray("c")
+                        ) == false
+                    ) throw Exception()
+                    (batchJsonArray[i / batchSize] as JSONArray).put(dataCatalysts[j])
+                }
+            }
+            return batchJsonArray
+        }
+
+        @Suppress("SameParameterValue")
+        private fun insertCatalysts(batchSize: Int, progressAll: Int, batchJsonArray: JSONArray) {
+            var progressStep: Float
+            for (i in 0 until (batchJsonArray.length())) {
+                database.insertCatalysts(batchJsonArray[i] as JSONArray)
+                progressStep = ((i * batchSize).toFloat() / progressAll.toFloat()) * (100).toFloat()
+                runOnUiThread {
+                    onProgressUpdate(progressStep)
+                }
+            }
+        }
         //endregion
 
         //region methods of run
         private fun onPreExecute() {
-
+            UserInterface.changeStatusLayout(
+                activityUpdateBinding.linearLayout,
+                false
+            )
+            stopThreadUpdateProgressOfDownloadThumbnail()
+            while (threadUpdateProgressOfDownloadThumbnail != null) {
+                Thread.sleep(100)
+            }
+            activityUpdateBinding.progressBar.progress = 0
+            activityUpdateBinding.notification.setTextColor(Configuration.COLOR_SUCCESS)
+            activityUpdateBinding.notification.text = Configuration.UPDATE_WAIT
         }
 
         private fun doInBackground(): ProcessStep {
-
-            return ProcessStep.NONE
+            try {
+                val (countDatabase, countSpreadsheet) = getCountOfCatalyst(fullUpdate)
+                if (fullUpdate) {
+                    database.resetDatabase()
+                } else {
+                    if (countDatabase == countSpreadsheet) {
+                        return ProcessStep.SUCCESS
+                    }
+                }
+                val batchSize = 100
+                val batchJsonArray = getBatchJsonArrayOfUpdate(batchSize, countDatabase)
+                val progressAll: Int = (countSpreadsheet - countDatabase)
+                insertCatalysts(batchSize, progressAll, batchJsonArray)
+                return ProcessStep.SUCCESS
+            } catch (e: UnknownHostException) {
+                return ProcessStep.NETWORK_FAILED
+            } catch (e: Exception) {
+                return ProcessStep.UNHANDLED_EXCEPTION
+            }
         }
 
         private fun onPostExecute(processStep: ProcessStep) {
-
+            when (processStep) {
+                ProcessStep.NETWORK_FAILED -> {
+                    activityUpdateBinding.notification.setTextColor(
+                        Configuration.COLOR_FAILED
+                    )
+                    activityUpdateBinding.notification.text =
+                        Configuration.NETWORK_FAILED
+                }
+                ProcessStep.UNHANDLED_EXCEPTION -> {
+                    activityUpdateBinding.notification.setTextColor(
+                        Configuration.COLOR_FAILED
+                    )
+                    activityUpdateBinding.notification.text =
+                        Configuration.UPDATE_FAILED
+                }
+                ProcessStep.SUCCESS -> {
+                    isAvailableUpdateCatalyst = false
+                    activityUpdateBinding.progressBar.progress = 100
+                    activityUpdateBinding.notification.setTextColor(Configuration.COLOR_SUCCESS)
+                    activityUpdateBinding.notification.text = Configuration.UPDATE_SUCCESS
+                }
+                else -> {
+                    //
+                }
+            }
+            UserInterface.changeStatusLayout(
+                activityUpdateBinding.linearLayout,
+                true
+            )
         }
         //endregion
 
-        /*override fun run() {
+        override fun run() {
             runOnUiThread {
                 onPreExecute()
             }
             val processStep: ProcessStep = doInBackground()
             runOnUiThread {
                 onPostExecute(processStep)
-            }
-        }*/
-
-
-        override fun run() {
-            //--- onPreExecute
-            runOnUiThread {
-                UserInterface.changeStatusLayout(
-                    activityUpdateBinding.linearLayout,
-                    false
-                )
-
-                //zakoncz tamten wątek, aby rozpocząć ten...
-                //zaczekaj aż damten się nie zakończy
-                refreshingDatabase = false
-                while (refreshingIsWorking) {
-                    Thread.sleep(100)
-                }
-
-
-                activityUpdateBinding.progressBar.progress = 0
-                activityUpdateBinding.notification.setTextColor(Configuration.COLOR_SUCCESS)
-                activityUpdateBinding.notification.text =
-                    Configuration.UPDATE_WAIT
-            }
-            //--- doInBackground
-            var processStep: ProcessStep = ProcessStep.NONE
-            try {
-                var countDatabase = 0
-                val countSpreadsheet: Int = Spreadsheet.getCountCatalyst()
-                if (fullUpdate) {
-                    database.resetDatabase()
-                } else {
-                    countDatabase = database.getCountCatalyst()
-                    if (countDatabase == countSpreadsheet) {
-                        processStep = ProcessStep.SUCCESS
-                    }
-                }
-                if (processStep != ProcessStep.SUCCESS) {
-                    val progressAll: Int = (countSpreadsheet - countDatabase)
-                    var progressStep: Float
-                    val dataCatalysts: JSONArray = Spreadsheet.getDataCatalyst(countDatabase)
-                    val batchJsonArray = JSONArray()
-                    val batchSize = 100
-                    for (i in 0 until (dataCatalysts.length()) step batchSize) {
-                        if (batchJsonArray.isNull(i)) batchJsonArray.put(JSONArray())
-                        for (j in i until i + batchSize) {
-                            if (dataCatalysts.isNull(j)) break
-                            if (checkIfRowIsAvailable(
-                                    dataCatalysts.getJSONObject(
-                                        j
-                                    ).getJSONArray("c")
-                                ) == false
-                            ) throw Exception()
-                            (batchJsonArray[i / batchSize] as JSONArray).put(dataCatalysts[j])
-                        }
-                    }
-                    for (i in 0 until (batchJsonArray.length())) {
-                        database.insertCatalysts(batchJsonArray[i] as JSONArray)
-                        progressStep =
-                            ((i * batchSize).toFloat() / progressAll.toFloat()) * (100).toFloat()
-                        //--- onProgressUpdate
-                        runOnUiThread {
-                            activityUpdateBinding.progressBar.progress =
-                                progressStep.toInt()
-                            activityUpdateBinding.notification.setTextColor(
-                                Configuration.COLOR_SUCCESS
-                            )
-                            activityUpdateBinding.notification.text =
-                                Configuration.UPDATE_WAIT
-                        }
-                    }
-                    processStep = ProcessStep.SUCCESS
-                }
-            } catch (e: UnknownHostException) {
-                processStep = ProcessStep.NETWORK_FAILED
-            } catch (e: Exception) {
-                processStep = ProcessStep.UNHANDLED_EXCEPTION
-            }
-            //--- onPostExecute
-            runOnUiThread {
-                when (processStep) {
-                    ProcessStep.NETWORK_FAILED -> {
-                        activityUpdateBinding.notification.setTextColor(
-                            Configuration.COLOR_FAILED
-                        )
-                        activityUpdateBinding.notification.text =
-                            Configuration.NETWORK_FAILED
-                    }
-                    ProcessStep.UNHANDLED_EXCEPTION -> {
-                        activityUpdateBinding.notification.setTextColor(
-                            Configuration.COLOR_FAILED
-                        )
-                        activityUpdateBinding.notification.text =
-                            Configuration.UPDATE_FAILED
-                    }
-                    ProcessStep.SUCCESS -> {
-                        isAvailableUpdateCatalyst = false
-                        activityUpdateBinding.progressBar.progress = 100
-                        activityUpdateBinding.notification.setTextColor(
-                            Configuration.COLOR_SUCCESS
-                        )
-                        activityUpdateBinding.notification.text =
-                            Configuration.UPDATE_SUCCESS
-                    }
-                    else -> {
-                        //
-                    }
-                }
-                UserInterface.changeStatusLayout(
-                    activityUpdateBinding.linearLayout,
-                    true
-                )
             }
         }
     }
