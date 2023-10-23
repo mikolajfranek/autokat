83bc1258989d6c7ec8f7d2daf41509719cc86ef5
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2023-10-23 23:36:19 +0200

    3.0.0 - read about TouchableHighlight elements

---
+++ CatBook/Shared/TouchableHighlights/BasicButton.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    TouchableHighlight
+} from 'react-native';
+
+export default function render() {
+    const [pressing, setPressing] = useState(false);
+
+    function _onPressIn() {
+        setPressing(true);
+    }
+
+    function _onPressOut() {
+        setPressing(false);
+    }
+
+    return (
+        <View style={styles.kontener}>
+            <TouchableHighlight
+                onPressIn={_onPressIn}
+                onPressOut={_onPressOut}>
+                <View style={styles.przycisk}>
+                    <Text style={styles.witaj}>
+                        {pressing ? 'Klik' : 'Naciśnij'}
+                    </Text>
+                </View>
+            </TouchableHighlight>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    kontener: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    przycisk: {
+        backgroundColor: 'lightblue',
+        borderRadius: 100,
+        height: 100,
+        width: 100,
+        justifyContent: 'center',
+    },
+    witaj: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+    },
+});
