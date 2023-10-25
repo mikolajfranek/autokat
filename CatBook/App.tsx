b134a248eb4429a749cf7ee30a0167e34ddb5741
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2023-10-25 22:12:24 +0200

    3.0.0 - read about FlatList

--- CatBook/App.tsx
+++ CatBook/App.tsx
@@ -6,15 +6,18 @@ import {
 } from 'react-native';
 import KoloPrzycisk from './Shared/TouchableHighlights/BasicButton';
 import KoloPrzesuwajace from './Shared/PanResponders/BasicButton';
+import ModelCatalyst from './Business/Models/Catalyst';
+
+const hide = false;
 
 export default function render() {
+  const model = new ModelCatalyst(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+  console.log('Assigned id is ' + model.id);
   return (
     <View style={styles.container}>
-      <KoloPrzycisk />
-      <KoloPrzesuwajace />
-      <Text>
-        Hello world!
-      </Text>
+      {hide ? <KoloPrzycisk /> : ''}
+      {hide ? <KoloPrzesuwajace /> : ''}
+      {hide ? <Text>Hello world!</Text> : ''}
     </View>
   );
 }
---
+++ CatBook/Business/Models/Catalyst.js
@@ -0,0 +1,25 @@
+export default class Catalyst {
+    constructor(id,
+        idPicture,
+        urlPicture,
+        thumbnail,
+        name,
+        brand,
+        platinum,
+        palladium,
+        rhodium,
+        type,
+        weight) {
+        this.id = id;
+        this.idPicture = idPicture;
+        this.urlPicture = urlPicture;
+        this.thumbnail = thumbnail;
+        this.name = name;
+        this.brand = brand;
+        this.platinum = platinum;
+        this.palladium = palladium;
+        this.rhodium = rhodium;
+        this.type = type;
+        this.weight = weight;
+    }
+}
