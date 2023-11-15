200fc0e4b1a910a55a3041e7946df6d35a2632fa
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2023-11-15 19:21:33 +0100

    3.0.0 - try implement universal button

--- CatBook/App.tsx
+++ CatBook/App.tsx
@@ -7,6 +7,7 @@ import {
 import KoloPrzycisk from './Shared/TouchableHighlights/BasicButton';
 import KoloPrzesuwajace from './Shared/PanResponders/BasicButton';
 import UICatalystFlatList from './Business/UI/CatalystFlatList';
+import Button from './Backend/Button/Button'
 
 const hide = false;
 
@@ -17,7 +18,12 @@ export default function render() {
       {hide ? <KoloPrzycisk /> : ''}
       {hide ? <KoloPrzesuwajace /> : ''}
       {hide ? <Text>Hello world!</Text> : ''}
-      <UICatalystFlatList />
+      {hide ? <UICatalystFlatList /> : ''}
+      <Button 
+        label={"hejo"}
+        onPress={null}
+        style={null}
+      />
     </View>
   );
 }
---
+++ CatBook/Backend/Button/style.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: { backgroundColor: "#FFDDFF", padding: 25, borderRadius: 5 }
+});
+
+export default styles;
+u
