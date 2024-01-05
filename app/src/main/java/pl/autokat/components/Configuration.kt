c4ef8b42204ded6537fe8b1f6d31253548af175f
Mikolaj Franek <m.franek@elka-it.pl>
2024-01-05 19:56:01 +0100

    1.0.8 - hotfixes before test

--- app/src/main/java/pl/autokat/components/Configuration.kt
+++ app/src/main/java/pl/autokat/components/Configuration.kt
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 class Configuration {
     companion object {
-        const val PRODUCTION: Boolean = true
+        const val PRODUCTION: Boolean = false
         val PROGRAM_MODE: ProgramMode = ProgramMode.COMPANY
         const val VERSION_APK: String = "1.0.8"
         const val DATABASE_VERSION_1_0_6: Int = 4
