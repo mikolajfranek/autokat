2de16cabebbb21c7f956a4be5733976cc8d4a4f8
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2024-01-24 10:03:56 +0100

    3.0.0 - work on strucrure

--- CatBook/Backend/GUI/MyButton/style.js
+++ CatBook/Backend/GUI/MyButton/style.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import * as GlobalStyle from '../../backend_style';
+import * as GlobalStyle from '../backend_style';
 
 const styles = StyleSheet.create({
     touchable: {
--- CatBook/Business/GUI/FormLogin/style.js
+++ CatBook/Business/GUI/FormLogin/style.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import * as GlobalStyle from './../../business_style';
+import * as GlobalStyle from '../business_style';
 
 const styles = StyleSheet.create({
     container: {
