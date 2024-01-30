bc9fe1125dc0e78d561eac95cbacac447660cf77
Mikolaj Franek <m.franek@elka-it.pl>
2024-01-30 14:12:57 +0100

    changing name of app (not all  places), lauching on android

--- CatBook/Business/index.tsx
+++ CatBook/Business/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {
+    Platform,
+
+} from 'react-native';
+
+
 import { View, Text } from '@gluestack-ui/themed';
 
-export default function render(): JSX.Element {
+const message = Platform.select({
+    ios: `Hello world in IOS ${Platform.Version}`,
+    android: `Hello world in Android ${Platform.Version}`,
+});
+
+export default function render(): React.JSX.Element {
     return (
-        <View p={"$4"}>
-            <Text>Hello world ;)</Text>
+        <View>
+            <Text>
+                {message}
+            </Text>
         </View>
     );
 }
--- CatBook/android/app/src/main/java/com/reactnativeapp/MainApplication.java
+++ CatBook/android/app/src/main/java/com/reactnativeapp/MainApplication.java
@@ -57,6 +57,7 @@ public class MainApplication extends Application implements ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    // java.lang.NoClassDefFoundError: Failed resolution of: Lcom/facebook/drawee/backends/pipeline/info/ImagePerfDataListener;
+    // ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 }
