0c9fe62371098f546be8d7a3ce0d8870ea489475
Mikolaj Franek <m.franek@elka-it.pl>
2024-01-31 22:59:56 +0100

    learning redux

--- CatBook/Business/store.tsx
+++ CatBook/Business/store.tsx
@@ -1,10 +1,17 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import counterReducer from './features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {
-    
+    counter: counterReducer,
   },
-})
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-//export type AppStore = typeof store;
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
