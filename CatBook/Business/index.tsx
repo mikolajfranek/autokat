1e23a5c372893b1a58079f995704625c29840a0d
Mikolaj Franek <m.franek@elka-it.pl>
2024-02-01 21:50:16 +0100

    Hello React Navigation and simple features

--- CatBook/Business/index.tsx
+++ CatBook/Business/index.tsx
@@ -1,15 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { Text, View } from 'react-native';
+import FormLogin from './Features/FormLogin';
+
+const Stack = createNativeStackNavigator();
 
 export default function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <View>
-        <Text>
-          Hello world
-        </Text>
-      </View>
+      <Stack.Navigator
+        initialRouteName="FormLogin"
+        screenOptions={{
+          headerStyle: { backgroundColor: 'lightblue' },
+        }}>
+        <Stack.Group>
+          <Stack.Screen
+            name="FormLogin"
+            component={FormLogin}
+            options={{ title: 'Formularz logowania' }} />
+        </Stack.Group>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
