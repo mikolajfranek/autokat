f978e80f6761b952cc3714fbe3f736ba4a95d9c9
Mikolaj Franek <m.franek@elka-it.pl>
2024-02-01 18:34:48 +0100

    create app, flow, jest

---
+++ CatBook/__tests__/App.test.tsx
@@ -0,0 +1,26 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: import explicitly to use the types shipped with jest.
+import { it, expect } from '@jest/globals';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
+
+it('unit test', () => {
+  expect(true).toBe(true);
+});
+
+it('snapshot test', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
