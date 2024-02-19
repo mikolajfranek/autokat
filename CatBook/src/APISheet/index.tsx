959b2c7cc57ce4276a769e3e9486f250ecc21813
Mikołaj Franek <m.franek@elka-it.eu>
2024-02-19 12:51:59 +0100

    add simple prepareHeaders redux

--- CatBook/src/APISheet/index.tsx
+++ CatBook/src/APISheet/index.tsx
@@ -2,7 +2,17 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSheet = createApi({
     reducerPath: 'apiSheet',
-    baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: '/fakeApi',
+        prepareHeaders: (headers, { getState }) => {
+            const token = null; //(getState() as RootState).auth.token;
+            // If we have a token set in state, let's assume that we should be passing it.
+            if (token) {
+                headers.set('authorization', `Bearer ${token}`);
+            }
+            return headers;
+        },
+    }),
     endpoints: builder => ({
         getPosts: builder.query({
             query: () => '/posts'
