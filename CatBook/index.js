abebfd6e06b3d7fb2e6d60922bd2c45a2a3af239
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2024-03-29 10:40:50 +0100

    react-native-paper on ios

--- CatBook/index.js
+++ CatBook/index.js
@@ -4,9 +4,21 @@ import { name } from './app.json';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
 import App from './src';
+import { MD3DarkTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: 'tomato',
+        secondary: 'yellow',
+    },
+};
 
 AppRegistry.registerComponent(name, () => () => (
     <Provider store={store}>
-        <App />
+        <PaperProvider theme={theme}>
+            <App />
+        </PaperProvider>
     </Provider>
 ));
--- CatBook/src/GUI/FormularzLogowania/index.tsx
+++ CatBook/src/GUI/FormularzLogowania/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { useAppDispatch } from '../../hooks';
 import { setAuthStatus } from '../../Slices/Auth';
+import { Button } from 'react-native-paper';
 
 export default function App(): React.JSX.Element {
     const dispatch = useAppDispatch();
@@ -11,11 +12,13 @@ export default function App(): React.JSX.Element {
                 Hello CatBook FormularzLogowania
             </Text>
             <Button
+                icon='camera'
                 onPress={async () => {
                     //TODO
                     dispatch(setAuthStatus(true));
-                }}
-                title="Zaloguj się" />
+                }}>
+                Zaloguj się
+            </Button>
         </View>
     );
 }
