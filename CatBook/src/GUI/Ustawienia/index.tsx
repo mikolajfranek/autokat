2c8c86feabe4226a1100da278cd2dcf10ce03d5f
Mikołaj Franek <m.franek@elka-it.eu>
2024-04-15 07:46:23 +0200

    packges, settings

--- CatBook/src/GUI/Ustawienia/index.tsx
+++ CatBook/src/GUI/Ustawienia/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { View } from 'react-native';
 import { setAuthenticated } from '../../Slices/Auth';
 import { useAppDispatch } from '../../hooks';
 import { Button, Switch, Text } from 'react-native-paper';
@@ -10,37 +10,18 @@ export default function App(): React.JSX.Element {
     const { toggleTheme, isThemeDark } = React.useContext(PreferencesContext);
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
-            <Switch
-                style={{ alignSelf: 'center' }}
-                color={'red'}
-                value={isThemeDark}
-                onValueChange={toggleTheme}
-            />
-
-            <Text style={{ alignSelf: 'center' }} >
-                Hello CatBook Ustawienia
-            </Text>
-
-            <Button icon={require('./chameleon.jpg')}
-                theme={{ colors: { primary: undefined } }} >
-                Is black text
-                <Text>Success too</Text>
-            </Button>
-
-            <Button
-                icon={() => (
-                    <Image
-                        source={require('./chameleon.jpg')}
-                        style={{ width: 100, height: 100 }}
-                    />
-                )}
-            >
-                Success
-            </Button>
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                <Text>Ciemny layout</Text>
+                <Switch
+                    style={{ alignSelf: 'center' }}
+                    color={'red'}
+                    value={isThemeDark}
+                    onValueChange={toggleTheme}
+                />
+            </View>
             <Button
                 icon='logout'
                 onPress={async () => {
-                    //TODO
                     dispatch(setAuthenticated(false))
                 }}>
                 Wyloguj się
